41118fa6607ef7318aa890c7e932df2c763699e8
abdulawalemon1 <emondx4@gmail.com>
2022-05-21 01:45:16 +0600

    jwt issued

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
+
 
 
 
@@ -24,7 +26,23 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db('doctors-portal').collection('services');
         const bookingCollection = client.db('doctors-portal').collection('bookings');
+        const userCollection = client.db('doctors-portal').collection('users');
 
+
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+
+        })
 
 
         app.get('/service', async (req, res) => {
@@ -34,19 +52,24 @@ async function run() {
             res.send(services);
         })
 
+
+        //This is not the proper way to query
+
         app.get('/available', async (req, res) => {
-            const date = req.query.date || 'May 20, 2022';
+            const date = req.query.date;
             //step-1 get all services
             const services = await serviceCollection.find().toArray();
 
             // step:2 get the booking of the day
             const query = { date: date };
             const bookings = await bookingCollection.find(query).toArray();
-            //step-3 for each servvice find bookings for that service
+
+            //step-3 for each service find bookings for that service
             services.forEach(service => {
                 const serviceBooking = bookings.filter(b => b.treatment === service.name);
                 const booked = serviceBooking.map(s => s.slot);
                 const available = service.slots.filter(s => !booked.includes(s));
+                service.slots = available;
 
 
             })
@@ -60,8 +83,19 @@ async function run() {
          * app.get('/booking') // add a new booking
          * app.post('/booking/') add a new booking
          * app.patch('/booking/:id') update one 
+         * app.put("/booking/:id") //upsert===> update(if exost) or insert(if doesnt exist)
+         * app.delete("/booking/:id") 
          * 
          */
+
+
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
